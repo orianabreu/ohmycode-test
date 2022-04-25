a0e0fed399f7dfda544640b3529d7b4424ae6864
Oriana Abreu <oriabreu.v@gmail.com>
2022-04-25 14:24:11 +0200

    set global theme

---
+++ src/theme/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+  palette: {
+    darkGrey: "hsl(235, 21%, 11%)",
+    darkGreyDesaturated: "hsl(235, 24%, 19%)",
+    lightGrey: "hsl(0, 0%, 98%)",
+  },
+  sizing: {
+    h1: "3rem",
+    h2: "2.5rem",
+    h3: "2rem",
+    subtitle: "1.5rem",
+    paragraph: "1rem",
+  },
+};
+
+export const media = {
+  tablet: `@media screen and (max-width: 768px)`,
+  mobile: `@media screen and (max-width: 480px)`,
+};
+
+export default theme;
