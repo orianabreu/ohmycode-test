c73d67de27d5da43496cc7afac45fce815efbc38
Oriana Abreu <oriabreu.v@gmail.com>
2022-04-28 02:32:37 +0200

    create menu comp and create todos page, add routes

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import * as S from "./Menu.styles";
+
+export default function AppMenu() {
+  return (
+    <S.NavBar>
+      <Link to='/'>Todos List</Link>
+      <Link to='/create-your-todos'>Create your Todos</Link>
+    </S.NavBar>
+  );
+}
