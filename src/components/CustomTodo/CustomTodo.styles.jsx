154bc392b040a6d69e1253ce59156b5c3ff0deed
Oriana Abreu <oriabreu.v@gmail.com>
2022-04-28 18:13:34 +0200

    create todos responsive

--- src/components/CustomTodo/CustomTodo.styles.jsx
+++ src/components/CustomTodo/CustomTodo.styles.jsx
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 import { TodoBox, IconContainer } from '../Todo/Todo.styles';
+import { media } from '../../theme/theme';
 
 export const CustomTodoBox = styled(TodoBox)`
-    grid-template-columns: 15% auto 20% 20%;
-    border-bottom: none;
+  grid-template-columns: 15% auto 20% 20%;
+  border-bottom: none;
+
+  ${media.mobile} {
+    grid-template-columns: 20% auto 25% 30%;
+  }
 `;
 
 export const DeleteBtnContainer = styled(IconContainer)`
-    background: none;
-    border: 1px solid ${({ theme }) => theme.palette.blue};
-    cursor: pointer;
+  background: none;
+  border: 1px solid ${({ theme }) => theme.palette.blue};
+  cursor: pointer;
 `;
-
-
-
